e69c6d7429f24cc6a0d55a997a837232a10271c6
ElonMusk-Real <dzulkarnaenaziz@gmail.com>
2020-02-06 21:34:53 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "../src/Components/Navbar/Navbar";
+import Section from "./Components/Section/Section.js";
+import ArrayMap from "./Components/ArrayMap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          title: "title",
+          nocard: true,
+          content: "haskfjhakshbfkjahsfkjbasjfagsfbasjkfjk",
+          theme: 1,
+          className: "section1"
+        },
+        {
+          title: "title2",
+          nocard: true,
+          content: "ajsfhkjabsjfkbajksfbjkasb",
+          theme: 2,
+          className: "section1"
+        },
+        {
+          title: "title2",
+          nocard: true,
+          content: "kafklahisfkjnjkasnbfkjansfjk",
+          theme: 3,
+          className: "section1"
+        },
+        {
+          title: "title2",
+          nocard: true,
+          content: "kafklahisfkjnjkasnbfkjansfjk",
+          theme: 3,
+          className: "section1"
+        },
+        
+      ]
+    };
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <Navbar />
+        <ArrayMap data={data}>
+          {({ title, className, nocard, content, theme }) => (
+            <Section
+              title={title}
+              showList={nocard}
+              className={className}
+              theme={theme}
+              content={content}
+            />
+          )}
+        </ArrayMap>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Homepage;
